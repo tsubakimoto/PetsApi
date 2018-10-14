05a01549c316cef831623ab1624d99e2070a7c25
tsubakimoto <tkmbmbr@gmail.com>
2018-10-14 11:24:24 +0900

    Initial

---
+++ PetsApi/Controllers/PetsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PetsApi.Models;
+
+//[assembly:ApiConventionType(typeof(DefaultApiConvention))]
+namespace PetsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetsController : ControllerBase
+    {
+        private readonly PetsContext _context;
+
+        public PetsController(PetsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pets
+        [HttpGet]
+        public IEnumerable<Pet> GetPet()
+        {
+            return _context.Pet;
+        }
+
+        // GET: api/Pets/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPet([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var pet = await _context.Pet.FindAsync(id);
+
+            if (pet == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pet);
+        }
+
+        // PUT: api/Pets/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPet([FromRoute] int id, [FromBody] Pet pet)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+
+            if (id != pet.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pet).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PetExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pets
+        [HttpPost]
+        public async Task<IActionResult> PostPet([FromBody] Pet pet)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Pet.Add(pet);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPet", new { id = pet.Id }, pet);
+        }
+
+        // DELETE: api/Pets/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePet([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var pet = await _context.Pet.FindAsync(id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pet.Remove(pet);
+            await _context.SaveChangesAsync();
+
+            return Ok(pet);
+        }
+
+        private bool PetExists(int id)
+        {
+            return _context.Pet.Any(e => e.Id == id);
+        }
+    }
+}
